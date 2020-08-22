794714610ad1b26c9e6ca1f060b791cb6c6d669e
Константин Дорохов <60038572+TenTax@users.noreply.github.com>
2020-08-22 12:49:05 +0300

    Add files via upload

---
+++ precache-manifest.32c5b3969b99c56302b9919c1c27ab4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9927803f143ec200bc9ba361ea59ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a834d0bd03e685918abb",
+    "url": "/static/css/main.652f76f9.chunk.css"
+  },
+  {
+    "revision": "7bf3ea65e396d8a9666c",
+    "url": "/static/js/2.65d0f303.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65d0f303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a834d0bd03e685918abb",
+    "url": "/static/js/main.ab4123c0.chunk.js"
+  },
+  {
+    "revision": "4f107a5c64cb62dc80e8",
+    "url": "/static/js/runtime-main.142d2794.js"
+  },
+  {
+    "revision": "3ee1f4017f6f5aa59944c590c77734f5",
+    "url": "/static/media/spinner.3ee1f401.gif"
+  },
+  {
+    "revision": "b78d98f81b6b5e12a505988172041451",
+    "url": "/static/media/trash.b78d98f8.png"
+  }
+]);
